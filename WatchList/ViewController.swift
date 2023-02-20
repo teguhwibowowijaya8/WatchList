68060a6c4a89ce2f935100d338f34afe1502b1e4
Teguh Wibowo Wijaya <teguhwibowowijaya8@gmail.com>
2023-02-20 10:33:25 +0700

    Initial Commit

---
+++ WatchList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchList
+//
+//  Created by Teguh Wibowo Wijaya on 20/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
